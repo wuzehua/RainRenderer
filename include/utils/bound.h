61b765fe3722aca660f7a2f2380a255cde305115
RainZhong <1322030678@qq.com>
2019-11-29 11:06:25 +0800

    add rays and bounding box

--- include/utils/bound.h
+++ include/utils/bound.h
@@ -5,4 +5,60 @@
 #ifndef RAINRENDERER_BOUND_H
 #define RAINRENDERER_BOUND_H
 
+#include <math/vector.h>
+
+namespace rzpbr{
+
+    template<typename T>
+    class Bound2{
+    public:
+        Bound2(){
+            T max = std::numeric_limits<T>::max();
+            T min = std::numeric_limits<T>::lowest();
+            minP = Point2<T>(min,min);
+            maxP = Point2<T>(max,max);
+        }
+
+        Bound2(const Point2<T>& p): minP(p),maxP(p){}
+        Bound2(const Point2<T>& p1, const Point2<T>& p2){
+            minP = Point2<T>(std::min(p1.x(), p2.x()), std::max(p1.y(), p2.y()));
+            maxP = Point2<T>(std::max(p1.x(),p2.x()), std::max(p1.y(), p2.y()));
+        }
+
+        const Point2<T>& operator[](int index) const {
+            assert(index == 0 || index == 1);
+            return (index == 0) ? minP : maxP;
+        }
+
+        bool operator==(const Bound2<T>& b) const {
+            return minP == b.minP && maxP == b.maxP;
+        }
+
+        bool operator!=(const Bound2<T>& b) const {
+            return minP != b.minP || maxP != b.maxP;
+        }
+
+        Vector2<T> diagonal(){
+            return maxP - minP;
+        }
+
+        T area(){
+            auto d = maxP - minP;
+            return (d.x() * d.y());
+        }
+
+
+
+
+        Point2<T> minP,maxP;
+    };
+
+    class Bound3{
+    public:
+
+        Point3<T> minP,maxP;
+    };
+}
+
+
 #endif //RAINRENDERER_BOUND_H
--- include/utils/ray.h
+++ include/utils/ray.h
@@ -17,8 +17,16 @@ namespace rzpbr {
 
     public:
         Ray(): tMax(Infinity),medium(nullptr),time(0.0){}
+        Ray(const Point3f& origin, const Vector3f& direction, Float tMax = Infinity, Float time = 0.0, const Medium* medium = nullptr):
+                origin(origin), direction(direction), tMax(tMax), time(time), medium(medium)
+        {}
 
+        Point3f operator()(Float t) const { return origin + t * direction;}
 
+        friend std::ostream& operator<<(std::ostream& os, const Ray& ray){
+            os<<"Ray( origin: "<<ray.origin<<", direction: "<<ray.direction<<", tMax:"<<ray.tMax<<", time: "<<ray.time<<") \n";
+            return os;
+        }
 
         Point3f origin;
         Vector3f direction;
@@ -26,6 +34,32 @@ namespace rzpbr {
         Float time;
         std::shared_ptr<const Medium> medium;
     };
+
+    class RayDifferential: public Ray{
+
+    public:
+        RayDifferential(): Ray(){ hasDifferential = false;}
+        RayDifferential(const Point3f& origin, const Vector3f& direction, Float tMax = Infinity, Float time = 0.0, const Medium* medium = nullptr):
+                Ray(origin, direction, tMax, time, medium)
+        {
+            hasDifferential = false;
+        }
+        RayDifferential(const Ray& ray): Ray(ray){
+            hasDifferential = false
+        }
+
+        void scaleDifferentials(Float scale){
+            rxOrigin = origin + (rxOrigin - origin) * scale;
+            ryOrigin = origin + (ryOrigin - origin) * scale;
+            rxDirection = direction + (rxDirection - direction) * scale;
+            ryDirection = direction + (ryDirection - direction) * scale;
+        }
+
+        bool hasDifferential;
+        Point3f rxOrigin,ryOrigin;
+        Vector3f rxDirection,ryDirection;
+    };
+
 }
 
 #endif //RAINRENDERER_RAY_H
